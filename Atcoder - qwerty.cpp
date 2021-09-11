d98c62326498f391dd1571eddca59e3f20ed26f4
Md. Farhan Sadiq <mdfarhansadiq01@gmail.com>
2021-09-12 00:15:11 +0600

    CODE

---
+++ Atcoder - qwerty.cpp
@@ -0,0 +1,28 @@
+///******* In the name of Allah *******///
+
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define nl '\n'
+void Ok()
+{
+    long int a, b, c, d, e, f, g, h, t, i, j, k, l, m, n, Mn, Mx;
+    long int ans_1, ans_2, ans_3, cnt_1, cnt_2, cnt_3, flag_1, flag_2, flag_3, temp, sum;
+    double aa, bb, cc, dd, ee, ff, gg;
+    char aaa, bbb, ccc, ddd;
+    string spc, in, keyword;
+
+    fastio;
+
+    for(i=1;i<=26;i++)
+      cin>>a, aaa = 96 + a, cout<<aaa;
+    
+    return;
+}
+
+int main()
+{
+  	Ok();
+  
+  	return 0;
+}  
